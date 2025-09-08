d93bebcb1f76e1546dbc972c6131114e9938562f
GH-LinCoder <LinCoder@mail.com>
2025-09-08 22:25:01 +0100

    Update sectionMutator.js

--- dash/mutate/sectionMutator.js
+++ dash/mutate/sectionMutator.js
@@ -1,15 +1,16 @@
+// needs container
 
 mutation.cards.forEach(cardData => {
   const card = generateCard(cardData);
   sectionContainer.appendChild(card);
 });
 
-//there is an previous version on file
+//there is an previous version on file - no there isn't. (is it at the Lab?)
 
 function generateCard({ name, description, action }) {
   const card = document.createElement('div');
   card.classList.add('dashboard-card');
-  card.dataset.action = action;
+  card.dataset.action = action;  // <--- applies new data-action
   card.innerHTML = `
     <h3>${name}</h3>
     <p>${description}</p>
