e1fdbb62d44359676c2fb402a2b425e39d94ec25
GH-LinCoder <LinCoder@mail.com>
2025-11-06 14:52:39 +0100

    Add imports to surveyBase.js
    copied from createSurvey

--- surveys/surveyBase.js
+++ surveys/surveyBase.js
@@ -1,5 +1,11 @@
 //   ./surveys/surveyBase.js
-// import{  } from '   ';
-
+import { appState} from '../state/appState.js';
+import { getClipboardItems, onClipboardUpdate } from '../../utils/clipboardUtils.js';
+import{executeIfPermitted} from '../registry/executeIfPermitted.js';
+import{showToast} from '../ui/showToast.js';
+import { petitionBreadcrumbs } from'../../ui/breadcrumb.js';
+import {icons} from '../registry/iconList.js';
 
 console.log('surveyBase.js loaded');
+
+
