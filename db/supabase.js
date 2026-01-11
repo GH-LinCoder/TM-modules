f9cb9d8d385959155f3ce68861a58a5ab85d352c
GH-LinCoder <LinCoder@mail.com>
2026-01-11 11:51:44 +0000

    use env variables
    Updated Supabase client configuration to use environment variables.

--- db/supabase.js
+++ db/supabase.js
@@ -4,9 +4,9 @@
 console.log('Imported: supabase.js');
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = 'https://kxdplsvojxgdskbbfonp.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4ZHBsc3ZvanhnZHNrYmJmb25wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzNjk4MzMsImV4cCI6MjA2ODk0NTgzM30.cbJAOYkEkNyd3VbgJQLEtWtWs_MKrpzA16ZHMuOP4bk'
 
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey =import.meta.env.VITE_SUPABASE_ANON_KEY
 let supabaseClient;
 
 export function createSupabaseClient() {
